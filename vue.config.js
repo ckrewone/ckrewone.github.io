61fcc5daa49f36c3432505b169065481f7695a9a
Michal Zakowski <ckrewone@o2.pl>
2019-09-01 10:47:11 -0400

    fixed vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   transpileDependencies: ['/node_modules[/\\\\]vuetify[/\\\\]/'],
-  publicPath: process.env.NODE_ENV === 'production' ? './C:/ProgramData/GSD Software/DOCUframe/Modules/GSD-Pool/dist/' : './'
 }

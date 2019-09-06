e53f811f058ff7319312d01408879c8642d8126f
Michal Zakowski <ckrewone@o2.pl>
2019-09-06 22:05:29 +0200

    created about and project layouts

--- src/components/Info/About.vue
+++ src/components/Info/About.vue
@@ -1,27 +1,103 @@
 <template>
-  <v-flex href="#about" id="about" class="about">
-    <h1>O mnie</h1>
-    <p>lorem ipsum dolar amet</p>
+  <v-flex href="#about" id="about" ref="about" class="about">
+    <v-flex class="parallelogram">
+      <v-flex class="parallelogram__1 parallelogram--absolute"></v-flex>
+      <v-flex class="parallelogram__2 parallelogram--absolute"></v-flex>
+      <v-flex class="parallelogram__3 parallelogram--absolute"></v-flex>
+      <v-flex class="parallelogram__4 parallelogram--absolute"></v-flex>
+      <v-flex clas="title">{{this.offsetTop}}</v-flex>
+    </v-flex>
   </v-flex>
 </template>
 
 <script>
-import CustomDiv from '../CustomDiv'
+import { mapGetters } from 'vuex'
 export default {
   name: 'About',
-  components: { CustomDiv }
+  data () {
+    return {
+      offsetTop: null
+    }
+  },
+  computed: {
+    ...mapGetters('style', ['pageY'])
+  },
+  mounted () {
+    this.offsetTop = this.$refs['about'].offsetTop
+  }
 }
 </script>
 
 <style lang="scss">
   .about {
-    padding-top: 100px;
-    z-index: -1;
-    top: 120vh;
-    left: 0;
+    margin-top: 150vh;
+    height: 100vh;
     position: absolute;
-    height: 100%;
-    width: 1000px;
-    background: #00aef6;
+    width: 100vw;
+  }
+  .parallelogram{
+    margin: auto;
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%,-50%);
+    width: 1300px;
+    height: 600px;
+    &--absolute{
+      position: absolute;
+    }
+    &__1{
+      -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -ms-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -o-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      z-index: 10;
+      width: 240px;
+      height: 500px;
+      margin-top: 50px;
+      margin-left: 160px;
+      transform: skew(20deg);
+      background: #00aef6;
+      border-radius: 3px;
+    }
+    &__2{
+      z-index: 9;
+      width: 150px;
+      height: 500px;
+      margin-top: 50px;
+      margin-left: 385px;
+      transform: skew(-10deg);
+      background: #0d47a1;
+      border-radius: 3px;
+    }
+    &__3{
+      -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -ms-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -o-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      z-index: 10;
+      width: 300px;
+      height: 600px;
+      margin-top: 50px;
+      margin-left: 538px;
+      transform: skew(20deg);
+      background: #00aef6;
+      border-radius: 3px;
+    }
+    &__4{
+      z-index: 9;
+      width: 100px;
+      height: 700px;
+      margin-top: -50px;
+      margin-left: 1048px;
+      transform: skew(-30deg);
+      background: #0d47a1;
+      border-radius: 3px;
+    }
+  }
+  .title{
+    position: absolute;
+    margin-top: 200px;
   }
 </style>
--- src/components/Info/Projects.vue
+++ src/components/Info/Projects.vue
@@ -1,8 +1,13 @@
 <template>
-  <v-flex href="#projects" id="projects" class="projects">
-    <h1>Projekty</h1>
-    <p>lorem ipsum dolar amet</p>
-  </v-flex>
+    <v-flex href="#projects" id="projects" class="projects">
+        <v-flex class="parallelogram">
+            <v-flex class="parallelogram__5 parallelogram--absolute"></v-flex>
+            <v-flex class="parallelogram__6 parallelogram--absolute"></v-flex>
+            <v-flex class="parallelogram__7 parallelogram--absolute"></v-flex>
+            <v-flex class="parallelogram__8 parallelogram--absolute"></v-flex>
+            <v-flex class="recentage parallelogram--absolute"></v-flex>
+        </v-flex>
+    </v-flex>
 </template>
 
 <script>
@@ -11,15 +16,100 @@
   }
 </script>
 
-<style scoped>
-  .projects {
-    padding-top: 100px;
-    z-index: -1;
-    top: 240vh;
-    left: 0;
-    position: absolute;
-    height: 100%;
-    width: 800px;
-    background: #aedd00;
-  }
+<style lang="scss">
+    .projects {
+        top: 300vh;
+        left: 0;
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        margin-top: -50px;
+    }
+
+    .parallelogram {
+        margin: auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 1300px;
+        height: 600px;
+
+        &--absolute {
+            position: absolute;
+        }
+
+        &__5 {
+            z-index: 8;
+            width: 100px;
+            height: 500px;
+            margin-top: 150px;
+            margin-left: 385px;
+            transform: skew(40deg);
+            background: #257a16;
+            border-radius: 3px;
+        }
+
+        &__6 {
+            -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -ms-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -o-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            z-index: 10;
+            width: 200px;
+            height: 700px;
+            margin-top: -50px;
+            margin-left: 700px;
+            transform: skew(-17deg);
+            background: #00c853;
+            border-radius: 3px;
+        }
+
+        &__7 {
+            z-index: 8;
+            width: 200px;
+            height: 550px;
+            margin-top: -50px;
+            margin-left: 854px;
+            transform: skew(10deg);
+            background: #257a16;
+            border-radius: 3px;
+        }
+
+        &__8 {
+            -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -ms-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            -o-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+            z-index: 10;
+            width: 300px;
+            height: 550px;
+            margin-top: -50px;
+            margin-left: 975px;
+            transform: skew(-15deg);
+            background: #00c853;
+            border-radius: 3px;
+        }
+    }
+
+    .recentage{
+      background: #bdbdbd;
+      width: 700px;
+      height: 500px;
+      margin-top: 50px;
+      margin-left: 50px;
+      z-index: 9;
+      -webkit-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -moz-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -ms-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      -o-box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+
+    .title {
+        position: absolute;
+        margin-top: 200px;
+    }
 </style>

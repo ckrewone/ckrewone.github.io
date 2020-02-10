9e7e25865e8bd0f200cc6841b4aba6524f5a1fdf
Michał Żakowski <michal@MacBook-Pro-Micha.local>
2020-02-10 19:42:40 +0100

    Fixed footre

--- src/components/Contact/ContactDesktop.vue
+++ src/components/Contact/ContactDesktop.vue
@@ -8,36 +8,35 @@
                 class="orange lighten-1 white--text text-xs-center"
                 flat
                 tile
-                style="width: 100%; height: 70vh"
+                style="width: 100%; height: 70vh;"
         >
-            <v-card-text>
+            <v-card-text class="white--text pt-0 text-md-center">
+                <v-flex style="margin-top: 20vh">
+                    <h1>Kontakt</h1>
+                </v-flex>
+            </v-card-text>
+            <v-card-text style="margin-top: 10vh">
                 <v-layout justify-center row>
                     <v-flex xs2>
                         <v-layout justify-center row>
                             <v-card class="buttons-group" @click="click" elevation="10" xs1>
-                                <v-img :src="fb" class="img" max-height="32" max-width="32" v-ripple/>
+                                <v-img :src="fb" class="img" v-ripple/>
                             </v-card>
-                            <v-card class="buttons-group text-md-center" xs1>
-                                <v-img :src="fb" class="img" max-height="32" max-width="32" v-ripple/>
+                            <v-card class="buttons-group text-md-center" elevation="10" xs1>
+                                <v-img :src="linkedin" class="img" v-ripple/>
                             </v-card>
-                            <v-card class="buttons-group text-md-center" xs1>
-                                <v-img :src="fb" class="img" max-height="32" max-width="32" v-ripple/>
+                            <v-card class="buttons-group text-md-center" elevation="10" xs1>
+                                <v-img :src="github" class="img" style="background: #fff" v-ripple/>
                             </v-card>
                         </v-layout>
                     </v-flex>
                 </v-layout>
             </v-card-text>
 
-            <v-card-text class="white--text pt-0 text-md-center">
-                <v-flex style="margin-top: 20vh">
-                    <h1>Kontakt</h1>
-                </v-flex>
-            </v-card-text>
-
             <v-divider></v-divider>
 
             <v-card-text class="white--text">
-                &copy;2019 — Made with 	&#x1F394; by <strong>Michał Żakowski</strong>
+                &copy;2019 — Made with love by <strong>Michał Żakowski</strong>
             </v-card-text>
         </v-card>
     </v-footer>
@@ -45,12 +44,16 @@
 
 <script>
 import fb from '../../assets/facebook.svg'
+import linkedin from '../../assets/linkedin-icon-2.svg'
+import github from '../../assets/github.svg'
 
 export default {
   name: 'ContactDesktop',
   data () {
     return {
-      fb
+      fb,
+      linkedin,
+      github
     }
   },
   methods: {
@@ -66,7 +69,8 @@ export default {
 
     .buttons-group {
         margin: 0 auto;
-        width: 32px;
+        background: #0d47a1;
+        width: 48px;
     }
 
     .img {
